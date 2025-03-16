3f8087e6ca5663f288337dd145e2b8b7b557db6c
ridanit-ruma <minenaturecrew@gmail.com>
2025-03-16 21:15:38 +0900

    NeisMealService 추가 및 PushService에 주입

--- src/push/push.module.ts
+++ src/push/push.module.ts
@@ -4,10 +4,11 @@ import { PushController } from './push.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
+import { NeisMealService } from 'src/neis-meal/neis-meal.service';
 
 @Module({
     imports: [ConfigModule, ScheduleModule],
-    providers: [PushService, PrismaService],
+    providers: [PushService, PrismaService, NeisMealService],
     controllers: [PushController],
 })
 export class PushModule {}
